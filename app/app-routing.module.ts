e1a0bdb09e88c6b0525fed93f18395b78635d384
Hendrik Rahardja <hendrik@alterra.id>
2020-12-22 16:39:03 +0700

    update routing documents

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomepageComponent } from './homepage/homepage.component';
-import { DocumentsComponent } from './documents/document.component'; 
+import { DocumentsComponent } from './documents/documents.component';
+
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomepageComponent },
